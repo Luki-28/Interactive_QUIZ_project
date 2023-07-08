9882535385981f310b2d6c2a21faa1ac3d309e11
Lukas <lukihaker1021@gmail.com>
2023-07-08 10:53:20 +0200

    small change

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const questions = document.querySelectorAll('.options')
-const submitButtons = document.querySelectorAll('.submit-btn')
-const score = document.querySelector('.correct-answers')
-const playAgain = document.querySelector('.play-again')
+const questions = document.querySelectorAll(".options")
+const submitButtons = document.querySelectorAll(".submit-btn")
+const score = document.querySelector(".correct-answers")
+const playAgain = document.querySelector(".play-again")
 
 let correctAnswers = 0
 let totalQuestions = questions.length
@@ -9,28 +9,28 @@ let question
 
 const checkAnswer = e => {
 	const currentQuestion = e.target.previousElementSibling
-	const selectedOption = currentQuestion.querySelector('.selected')
+	const selectedOption = currentQuestion.querySelector(".selected")
 
 	if (!selectedOption) {
 		return
 	}
 
-	const options = currentQuestion.querySelectorAll('.option')
+	const options = currentQuestion.querySelectorAll(".option")
 	options.forEach(option => {
 		option.disabled = true
 	})
-    
+
 	if (
-        selectedOption.textContent === 'Africa' ||
-		selectedOption.textContent === 'Zebra' ||
-		selectedOption.textContent === 'Paris'
-        ) {
-		selectedOption.classList.add('correct')
+		selectedOption.textContent === "Africa" ||
+		selectedOption.textContent === "Zebra" ||
+		selectedOption.textContent === "Paris"
+	) {
+		selectedOption.classList.add("correct")
 		correctAnswers++
 	} else {
-		selectedOption.classList.add('incorrect')
-		const correctOption = currentQuestion.querySelector('.correct')
-		correctOption.classList.add('correct')
+		selectedOption.classList.add("incorrect")
+		const correctOption = currentQuestion.querySelector(".correct")
+		correctOption.classList.add("correct")
 	}
 	score.textContent = correctAnswers
 
@@ -41,26 +41,26 @@ const checkAnswer = e => {
 const selectOption = e => {
 	const currentOption = e.target
 	const currentQuestion = currentOption.parentNode
-	const options = currentQuestion.querySelectorAll('.option')
+	const options = currentQuestion.querySelectorAll(".option")
 
 	options.forEach(option => {
-		option.classList.remove('selected')
+		option.classList.remove("selected")
 	})
 
-	currentOption.classList.add('selected')
+	currentOption.classList.add("selected")
 }
 
 const resetQuiz = () => {
-    correctAnswers = 0
-    score.textContent = correctAnswers
+	correctAnswers = 0
+	score.textContent = correctAnswers
 
-    questions.forEach(question => {
-        const options = question.querySelectorAll('.option')
-        options.forEach(option => {
-            option.disabled = false
-            option.classList.remove('selected', 'correct', 'incorrect')
-        })
-    })
+	questions.forEach(question => {
+		const options = question.querySelectorAll(".option")
+		options.forEach(option => {
+			option.disabled = false
+			option.classList.remove("selected", "correct", "incorrect")
+		})
+	})
 	submitButtons.forEach(button => {
 		const currentButton = button.target
 		currentButton.disabled = false
@@ -68,14 +68,14 @@ const resetQuiz = () => {
 }
 
 submitButtons.forEach(button => {
-	button.addEventListener('click', checkAnswer)
+	button.addEventListener("click", checkAnswer)
 })
 
 questions.forEach(question => {
-	const options = question.querySelectorAll('.option')
+	const options = question.querySelectorAll(".option")
 	options.forEach(option => {
-		option.addEventListener('click', selectOption)
+		option.addEventListener("click", selectOption)
 	})
 })
 
-playAgain.addEventListener('click', resetQuiz)
+playAgain.addEventListener("click", resetQuiz)
